66809ef264031280671615d335cbf67d20f3cfa0
kuatroka <aaarmani_2000@yahoo.com>
2024-10-10 01:36:09 +0100

    added tabs from flowbite and made it work when clicking

--- lib/phoenix_counter_web/live/presence_component.ex
+++ lib/phoenix_counter_web/live/presence_component.ex
@@ -3,7 +3,7 @@ defmodule PresenceComponent do
 
   def render(assigns) do
     ~H"""
-    <h1 class="text-center pt-2 text-xl">Connected Clients: <%= @present %></h1>
+    <h1 class="text-center pt-2 text-xl pb-4">Connected Clients: <%= @present %></h1>
     """
   end
 end
